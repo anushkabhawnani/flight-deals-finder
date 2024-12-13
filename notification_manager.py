58f8fdf720a9d24ad9ff2ce2eeaef7f59fe8fbe4
anushkabhawnani <anushkab1411@gmail.com>
2024-12-13 15:24:31 +0530

    Update notification_manager.py

--- notification_manager.py
+++ notification_manager.py
@@ -15,6 +15,7 @@ class NotificationManager:
     def __init__(self, flight_search: FlightSearch):
         self.cheapest_tickets = flight_search
         self.send_whatsapp()
+        self.send_email()
 
     def send_whatsapp(self):
 
@@ -28,3 +29,24 @@ class NotificationManager:
                 body=f'Low Price Alert! Only ₹{ticket['ticket_price']} to fly from BOM to {ticket['IATA']}, on {ticket['fromDate']} until {ticket['toDate']}',
                 to=WHATSAPP_NUMBER
             )
+
+    def send_email(self):
+
+        # SEND AN EMAIL TO EACH USER IN THE SUBSCRIPTION PLAN FOR EACH TICKET THAT IS CHEAP
+
+        for user in self.sheety.get_emails():
+
+            for ticket in self.cheapest_tickets.find_cheapest_tickets():
+                
+                with smtplib.SMTP('smtp.gmail.com') as connection:
+                    connection.starttls()
+
+                    connection.login(user=my_email, password=email_password)
+
+                    message = f'Subject: Low Price Alert!\n\nDear {user['firstName']} {user['lastName']},\n\nOnly ₹{ticket['ticket_price']} to fly from BOM to {ticket['IATA']}, on {ticket['fromDate']} until {ticket['toDate']}'
+
+                    connection.sendmail(
+                        from_addr=my_email,
+                        to_addrs=user['emailAddress'],
+                        msg=message.encode('utf-8').strip())
+
